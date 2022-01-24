b195958911b98238127867f5d40e073aaa20d4de
Jake Li <lijake@umich.edu>
2022-01-24 10:44:50 -0500

    second commit

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Grid from './components/Grid';
 import Keyboard from './components/Keyboard';
